3f1a44b6bddf9c31181d5fbbb9357cbb3b0d1547
Jornack <jornack@gmail.com>
2012-02-18 14:23:15 +0100

    removed comments

--- src/com/jornack/skyscraper/util/PreferenceManager.java
+++ src/com/jornack/skyscraper/util/PreferenceManager.java
@@ -2,18 +2,6 @@ package com.jornack.skyscraper.util;
 
 import java.util.prefs.Preferences;
 
-/**
- * <p>Title:		PreferencesManager</p><br>
- * <p>Description:	Preference Manager to read/write system tray app settings</p><br>
- * @author		    <a href="http://eric-blue.com">Eric Blue</a><br>
- *
- * $Date: 2011-07-24 17:54:27 $ 
- * $Author: ericblue76 $
- * $Revision: 1.2 $
- *
- */
-
-
 public class PreferenceManager {
 	
 	private static Preferences prefs = null;;
